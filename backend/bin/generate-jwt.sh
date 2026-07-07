cc7c66fe5660dcf53f8e1c15c6156e589580bf11
Juan Luis <gamito.juan@nimioestudio.com>
2026-07-07 10:35:04 +0200

    Añadir script para generar JWT automáticamente

--- backend/bin/generate-jwt.sh
+++ backend/bin/generate-jwt.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 cd /app/backend
+mkdir -p config/jwt
 if [ ! -f config/jwt/private.pem ]; then
-    mkdir -p config/jwt
     php bin/console lexik:jwt:generate-keypair --skip-if-exists
     chmod 644 config/jwt/*.pem
 fi
